e0c3c49d4e12406eaab86648eaf1c0fb658d0d7c
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-11-21 17:24:24 +0530

    created home link in app.js

--- src/App.js
+++ src/App.js
@@ -6,3 +6,23 @@ import Create from './Components/create.component';
 import Edit from './Components/edit.component';
 import Index from './Components/index.component';
 
+class App extends Component{
+  render(){
+    return(
+      <Router>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <Link to={'/'} className='navbar-brand'>React Crud Example</Link>
+            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+              <ul className='navbar-nav mr-auto'>
+                <li>
+                  <link to={'/'} className='nav-link'>HOME</link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </Router>
+    )
+  }
+}
