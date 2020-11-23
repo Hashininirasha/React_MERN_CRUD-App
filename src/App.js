bce6ffb7685a545104908b51883d2567c5a0f692
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-11-23 18:02:06 +0530

    moved Components file in to src file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowerRouter as Router,Switch,Route,Link} from 'react-router-dom'; 
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'; 
 
 import Create from './Components/create.component';
 import Edit from './Components/edit.component';
