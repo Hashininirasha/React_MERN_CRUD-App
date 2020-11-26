8f58816106e0596cc745d452cb1ebeda9371efb6
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-11-26 18:34:45 +0530

    import predefinded variables and methods in to create.component.js

--- src/Components/create.component.js
+++ src/Components/create.component.js
@@ -47,28 +47,42 @@ export default class Create extends Component {
         })
     }
 
-
-
     render(){
         return(
             <div style={{marginTop:10}}>
                 <h3>Add New Business</h3>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div className='form-group'>
                         <label>Add Person Name :</label>
-                        <input type='text' className='form-control'> </input>
+                        <input type='text' 
+                                className='form-control'
+                                value={this.state.person_Name}
+                                onChange={this.onChangePersonName}> 
+                                </input>
                     </div>
                     <div className='form-group'>
                         <label>Add Business Name :</label>
-                        <input type='text' className='form-control'> </input>
+                        <input type='text' className='form-control'
+                                className='form-control'
+                                value={this.state.business_name}
+                                onChange={this.onChangeBusinessName}> 
+                                 </input>
                     </div>
                     <div className='form-group'>
                         <label>Add NIC Number :</label>
-                        <input type='text' className='form-control'> </input>
+                        <input type='text' className='form-control'
+                                className='form-control'
+                                value={this.state.business_NIC}
+                                onChange={this.onChangeNIC}>
+                                 </input>
                     </div>
                     <div className='form-group'>
                         <label>Add Address :</label>
-                        <input type='text' className='form-control'> </input>
+                        <input type='text' className='form-control'
+                                className='form-control'
+                                value={this.state.business_address}
+                                onChange={this.onChangeAddress}>
+                                </input>
                     </div>
                     <div className='form-group'>
                         <input type='Submit' value='Register business' className='btn btn-primary'></input>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import {BrowserRouter} from "react-router-dom";
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>, 
   document.getElementById('root')
 );
 serviceWorker.unregister();
